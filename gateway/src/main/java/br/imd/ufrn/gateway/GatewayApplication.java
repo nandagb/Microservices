9963d260216c6d7ec3ca5c97970426983b23062f
nandagb <nandagb23@gmail.com>
2026-06-04 14:33:00 -0300

    feat: adicionando projeto gateway

---
+++ gateway/src/main/java/br/imd/ufrn/gateway/GatewayApplication.java
@@ -0,0 +1,26 @@
+package br.imd.ufrn.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+			.route(p -> p
+				.path("/get")
+				.filters(f -> f.addRequestHeader("Hello", "World"))
+				.uri("http://httpbin.org:80"))
+			.build();
+	}
+
+}
