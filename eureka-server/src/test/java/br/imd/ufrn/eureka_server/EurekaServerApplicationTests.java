8250a734903ddfc9169d95e867b15a15c9fe77aa
nandagb <nandagb23@gmail.com>
2026-06-04 19:38:16 -0300

    feat: adicionando eureka-server

---
+++ eureka-server/src/test/java/br/imd/ufrn/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.imd.ufrn.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
