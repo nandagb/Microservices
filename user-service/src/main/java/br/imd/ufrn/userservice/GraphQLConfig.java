4dd0e71e40922d3f189526089ddc7f774be1df52
nandagb <nandagb23@gmail.com>
2026-06-15 22:50:31 -0300

    feat: usando discovery do eureka para chamar ai-service no user-service

--- user-service/src/main/java/br/imd/ufrn/userservice/GraphQLConfig.java
+++ user-service/src/main/java/br/imd/ufrn/userservice/GraphQLConfig.java
@@ -7,11 +7,19 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class GraphQLConfig {
-    @Bean
-    HttpGraphQlClient aiGraphQlClient() {
 
-        WebClient webClient = WebClient.builder()
-                .baseUrl("http://localhost:8090/graphql")
+    @Bean
+    @org.springframework.cloud.client.loadbalancer.LoadBalanced
+    WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
+
+    @Bean
+    HttpGraphQlClient aiGraphQlClient( WebClient.Builder webClientBuilder ) {
+
+        WebClient webClient = webClientBuilder
+                // .baseUrl("http://localhost:8090/graphql")
+                .baseUrl("http://AI-SERVICE/graphql")
                 .build();
 
         return HttpGraphQlClient.create(webClient);
