c11a056950798462c4fd5d3ba8d3f47aa0eca941
nandagb <nandagb23@gmail.com>
2026-06-28 21:15:21 -0300

    feat: adicionando servico para serverless function

---
+++ function/src/test/java/br/imd/ufrn/function/FunctionApplicationTests.java
@@ -0,0 +1,13 @@
+package br.imd.ufrn.function;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
