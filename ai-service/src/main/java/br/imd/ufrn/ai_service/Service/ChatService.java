b91114f1fcdd9e5d7dcb23939c502bab5f47773d
nandagb <nandagb23@gmail.com>
2026-06-30 21:47:17 -0300

    adicionando rate limit client-side no ai-service

--- ai-service/src/main/java/br/imd/ufrn/ai_service/Service/ChatService.java
+++ ai-service/src/main/java/br/imd/ufrn/ai_service/Service/ChatService.java
@@ -11,6 +11,8 @@ import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.stereotype.Service;
 
 import br.imd.ufrn.ai_service.DAO.UsersDAO;
+import io.github.resilience4j.ratelimiter.RequestNotPermitted;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 
 @Service
 public class ChatService {
@@ -35,6 +37,7 @@ public class ChatService {
     }
 
     // With memory
+    @RateLimiter(name = "llm", fallbackMethod = "rateLimitFallback")
     public String getChatAnswer(String prompt) {
         return chatClient.prompt()
                 .advisors(
@@ -46,6 +49,10 @@ public class ChatService {
                 .user(prompt)
                 .call()
                 .content();
+    }
+
+    public String rateLimitFallback(String prompt, RequestNotPermitted ex) {
+        return "O limite de requisições para o modelo foi atingido. Tente novamente em alguns instantes.";
     }
 
     
